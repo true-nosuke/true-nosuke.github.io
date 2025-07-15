704b8bb6803f4feec76241d8d2eeee9f98d0e457
nosuke-true <popnosuke0902@gmail.com>
2025-07-15 21:29:19 +0900

    Add: Service WorkerとWeb App Manifestを追加

---
+++ asset/unity/ultimate_shiritori_english/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "The-ABC-Siritori-1.0";
+const contentToCache = [
+    "Build/Ultimate_Shiritori_English.loader.js",
+    "Build/Ultimate_Shiritori_English.framework.js.unityweb",
+    "Build/Ultimate_Shiritori_English.data.unityweb",
+    "Build/Ultimate_Shiritori_English.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
